fe41c5de6ef49c4b11d02278eeb67ccf0d27f899
root <root@localhost.localdomain>
2015-04-20 06:01:47 +0000

    Edit response message

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("hello!!")
+        self.wfile.write("hello! CI!")
         
     do_POST = do_GET    
     do_PUT = do_GET
