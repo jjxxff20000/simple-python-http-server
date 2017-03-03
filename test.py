cecf701ba544e131dc49d8f1cd3837f127fca979
jiaxf <jiaxf@clo.com.cn>
2017-03-03 16:35:03 +0800

    push a docker test

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("hello world!!")
+        self.wfile.write("hello my  world!!")
         
     do_POST = do_GET    
     do_PUT = do_GET
