25631420a106c89ad675690ba077fd90bc69558e
jiaxf <jiaxf@clo.com.cn>
2017-03-08 16:22:49 +0800

    push jenkins again test

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("hello my  world again!!")
+        self.wfile.write("hello my  world again &&& again now!!")
         
     do_POST = do_GET    
     do_PUT = do_GET
